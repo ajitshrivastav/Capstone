f3e1c7b220d0363b01d46541acc1c1d08a2e31c6
ajitshrivastav <ajitshrivastav@gmail.com>
2016-09-26 16:33:58 -0700

    Update pass1_gender.R

--- scripts/pass1_find_gender/pass1_gender.R
+++ scripts/pass1_find_gender/pass1_gender.R
@@ -1,107 +1,53 @@
 library(dplyr)
 library(stringr)
-# library('RCurl')
-# library('rjson')
-# 
-# #global map to save name -> gender map, don't want to repeat the operations of gender evaluation.
-# name_gender_map <- new.env()
-# 
-# 
-# findgender_web <- function(first_name){
-#   gender <- c()
-#   cat("num of elements ", length(first_name))
-#   for(idx in 1:length(first_name)){
-#     
-#     print(paste("Try getting gender from map for ", first_name[idx]))
-#     if(is.null(first_name[idx]) || nchar(first_name[idx]) < 3){
-#       gender[idx] <- NA
-#       next
-#     }
-#     g <- try(get(as.character(first_name[idx]), name_gender_map))
-#     if (class(g) == "try-error") {
-#       #url_string <- paste("https://gender-api.com/get?key=cGVLPhBfVJnXhggBgm&name=",first_name[idx],sep="")
-#       url_string <- paste("https://api.genderize.io/?name=",first_name[idx],sep="")
-#       person_info = getURL(url_string)
-#       if (class(person_info) == "try-error") {
-#         gender[idx] <- NA
-#       } else{
-#         person_info_json <- try(fromJSON(person_info))
-#         if (class(person_info_json) == "try-error") {
-#           gender[idx] <- NA
-#         } else{
-#           print(paste("json - ", person_info_json))
-#           if(is.null(person_info_json$gender)){
-#             gender[idx] <- NA
-#           } else {
-#             gender[idx] <- person_info_json$gender
-#             assign(as.character(first_name[idx]), person_info_json$gender, name_gender_map)
-#           }
-#         }
-#       }
-#       
-#     } else{
-#       gender[idx] = g
-#     }
-#     remove('g')
-#   }
-#   gender
-# }
+library(hashmap)
 
-name_gender_us_map <- new.env()
-name_gender_uk_map <- new.env()
+#data source - https://github.com/OpenGenderTracking/gender-api
+us_name_gender_ds <- read.csv("data_sets/input/misc/usprocessed_names.csv")
+uk_name_gender_ds <- read.csv("data_sets/input/misc/ukprocessed_names.csv")
 
-#https://github.com/OpenGenderTracking/gender-api
-us_name_gender_ds <- read.csv("/Users/ajitshrivastav/my_Files/study/Data_Science/CapStone/SF_Salary/data_sets/name_data_sets/clean/usprocessed_names.csv")
-uk_name_gender_ds <- read.csv("/Users/ajitshrivastav/my_Files/study/Data_Science/CapStone/SF_Salary/data_sets/name_data_sets/clean/ukprocessed_names.csv")
+# create vector to capture names / gender
+name_vector <- c()
+gender_vector <- c()
 
+# map name and gender to name/gendor vectors
 for(i in 1:nrow(us_name_gender_ds)) {
   row <- us_name_gender_ds[i,]
-  assign(as.character(tolower(row$Name)), tolower(row$prob.gender), name_gender_us_map)
+  name_vector <- append(name_vector, as.character(row$Name))
+  gender_vector <- append(gender_vector, as.character(row$prob.gender))
 }
 
 for(i in 1:nrow(uk_name_gender_ds)) {
   row <- uk_name_gender_ds[i,]
-  assign(as.character(tolower(row$Name)), tolower(row$prob.gender), name_gender_uk_map)
+  name_vector <- append(name_vector, as.character(row$Name))
+  gender_vector <- append(gender_vector, as.character(row$prob.gender))
 }
 
+# create hashmap for easy gender lookup based on name
+gender_map <- hashmap(name_vector, gender_vector)
+
+# method to evaluate gender based on first name
 findgender_local <- function(first_name){
-    gender <- c()
-    cat("num of elements ", length(first_name))
-    for(idx in 1:length(first_name)){
-      print(paste("Try getting gender from map for ", first_name[idx]))
-      if(is.null(first_name[idx]) || nchar(first_name[idx]) < 3){
-        gender[idx] <- NA
-        next
-      } else{
-        # try in US name map
-        g <- try(get(as.character(first_name[idx]), name_gender_us_map))
-        if (class(g) == "try-error") {
-          # try in UK name map
-          g <- try(get(as.character(first_name[idx]), name_gender_uk_map))
-          if (class(g) == "try-error") {
-            gender[idx] <- NA
-            next
-          } else{
-            r <- try(gender[idx] <- g)
-            if (class(r) == "try-error") {
-              gender[idx] <- NA
-            }
-          }
-        } else{
-          r <- try(gender[idx] <- g)
-          if (class(r) == "try-error") {
-            gender[idx] <- NA
-          }
-        }
-      }
-      remove('g')
+  # gender vector t be returned
+  gender <- c()
+  
+  # iterate through name vector
+  for(idx in 1:length(first_name)){
+    # set gender as NA if first name is not valid
+    if(is.null(first_name[idx]) || nchar(first_name[idx]) < 3){
+      gender[idx] <- NA
+      next
+    } else{
+      # lookup gender from the hashmap
+      gender[idx] <- gender_map$find(first_name[idx])
     }
+    # return gender vector
     gender
+  }
 }
 
 # Load csv file in a data frame
-original_salary <- read.csv("/Users/ajitshrivastav/my_Files/study/Data_Science/CapStone/SF_Salary/data_sets/Salaries.csv")
-original_salary$EmployeeName
+original_salary <- read.csv("data_sets/input/primary/Salaries.csv")
 # clean up employee names
 # trim spaces
 salary <- original_salary %>% mutate(EmployeeName=trimws(EmployeeName))
@@ -124,19 +70,6 @@ salary <- salary %>% mutate(last_name=substr(EmployeeName, unlist(gregexpr(patte
 #salary$last_name
 salary <- salary %>% mutate(gender=findgender_local(first_name))
 #salary$gender
-write.csv(salary, file = "/Users/ajitshrivastav/my_Files/study/Data_Science/CapStone/SF_Salary/data_sets/pass1_gender_salary.csv")
+write.csv(salary, file = "data_sets/intermediate/pass1_gender_salary.csv")
 
-# load data sets for first name, last name , gender from external sources
-
-# Loop through all the employee names, outer loop
-
-# Inner loop, loop through each data sets,
-# match, first name, match last name, if first name and last name match is found, break since it is a best match found
-# If either of the first name or last name is matched, then calculate the weightage, last name match has high 
-# weight than first name. Based on the match, assign race for the person
-# If no match found for both first/last name, assign NA 
-
-# based on the first name match, assign the gender for the person
-
-# Already have a data sets for Blacks/Whites/Indian/Hispanic. Need more data sets for Asian, 
 
